81fa2b808d1b8e5ae8b6a21c5e9cd286278877f1
amoraglio <albmor@gmail.com>
2018-09-21 15:39:55 +0100

    len trace takes into account size of trace entry of list type

--- src/solver/AUTO_PARAM.py
+++ src/solver/AUTO_PARAM.py
@@ -7,13 +7,13 @@ def avg_size_trace(tr):
     if not tr.randsol in AVG_SIZE:
 
         #print("computing avg size", end="")
-        curr_cum_size, curr_iter = sum([len(tr.get_trace()[1]) for _ in range(1000)]), 1000
+        curr_cum_size, curr_iter = sum([len_trace(tr.get_trace()[1]) for _ in range(1000)]), 1000
         curr_avg_size = 1.0 * curr_cum_size / curr_iter
         prev_avg_size = 0
         while abs(1-prev_avg_size/curr_avg_size) > 0.001: # coverged within 1% tolerance?
             #print(".", end="")
             prev_avg_size = curr_avg_size
-            curr_cum_size += sum([len(tr.get_trace()[1]) for _ in range(1000)])
+            curr_cum_size += sum([len_trace(tr.get_trace()[1]) for _ in range(1000)])
             curr_iter += 1000
             curr_avg_size = 1.0 * curr_cum_size / curr_iter
         
@@ -21,5 +21,10 @@ def avg_size_trace(tr):
         #print(" average trace size: ", int(curr_avg_size))
         
     return AVG_SIZE[tr.randsol]
+
+def len_trace(trace): # takes into account size of trace entry of list type
+    
+    return sum(len(trace[k][1]) if isinstance(trace[k][1], list) else 1 for k in trace.keys())
+
 
 ######
