a744d2eca9cdeebda85e8350b23f1fb02cfd1380
amoraglio <albmor@gmail.com>
2018-09-21 13:07:29 +0100

    bug fixed

--- src/solver/REP_PERM.py
+++ src/solver/REP_PERM.py
@@ -7,14 +7,12 @@ class PERM(REP):
     def __init__(self, *args):
         self.population, self.k = args
 
-    # FIXME do we need to copy output, ie perturbed_output = output[:],
-    # to avoid altering output itself?
     def perturb(self, entry):
         
         typ, output = entry
 
         swap = random.sample(range(len(output)), 2)
-        perturbed_output = output
+        perturbed_output = output[:]
         perturbed_output[swap[0]], perturbed_output[swap[1]] = perturbed_output[swap[1]], perturbed_output[swap[0]] # swap
 
         return (typ, perturbed_output)
